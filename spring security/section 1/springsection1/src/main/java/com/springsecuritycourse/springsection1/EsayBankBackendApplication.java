9669b7791fc7594ddc4abe09aa1583cbe72af6ae
dinaahmed10 <codedinaa@gmail.com>
2024-11-03 04:43:57 +0200

    section 1

--- spring security/section 1/springsection1/src/main/java/com/springsecuritycourse/springsection1/EsayBankBackendApplication.java
+++ spring security/section 1/springsection1/src/main/java/com/springsecuritycourse/springsection1/EsayBankBackendApplication.java
@@ -2,8 +2,10 @@ package com.springsecuritycourse.springsection1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan("com.springsecuritycourse.springsection1.contoller")
 public class EsayBankBackendApplication {
 
 	public static void main(String[] args) {
